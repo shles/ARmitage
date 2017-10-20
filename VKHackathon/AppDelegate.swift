a42c0095ae5d42bf289128881fabc082e73181fd
Timofey Solonin <tim-solonin2@yandex.ru>
2017-10-20 16:08:38 +0300

    Initial commit

---
+++ VKHackathon/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  VKHackathon
+//
+//  Created by Timofey on 10/20/17.
+//  Copyright © 2017 NFO. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
